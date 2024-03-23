3dfe016a2e689e4b0f8fe4244cad4ded7bef732a
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-03-23 14:50:11 +0000

    edited main.py to conform with PEP8 styles

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import matplotlib.pyplot as plt
 
 var = 0.2
 n = 800
-class_0_a = var * np.random.randn(n//4,2)
-class_0_b =var * np.random.randn(n//4,2) + (2,2)
+class_0_a = var * np.random.randn(n//4, 2)
+class_0_b = var * np.random.randn(n//4, 2) + (2, 2)
 
-class_1_a = var* np.random.randn(n//4,2) + (0,2)
-class_1_b = var * np.random.randn(n//4,2) +  (2,0)
+class_1_a = var * np.random.randn(n//4, 2) + (0, 2)
+class_1_b = var * np.random.randn(n//4, 2) + (2, 0)
 
-X = np.concatenate([class_0_a, class_0_b,class_1_a,class_1_b], axis =0)
-Y = np.concatenate([np.zeros((n//2,1)), np.ones((n//2,1))])
-X.shape, Y.shape
+X = np.concatenate([class_0_a, class_0_b, class_1_a, class_1_b], axis =0)
+Y = np.concatenate([np.zeros((n//2, 1)), np.ones((n//2, 1))])
 
 rand_perm = np.random.permutation(n)
 
@@ -21,17 +20,16 @@ Y = Y[rand_perm, :]
 
 X = X.T
 Y = Y.T
-X.shape, Y.shape
 
 # train test split
 ratio = 0.8
-X_train = X [:, :int (n*ratio)]
-Y_train = Y [:, :int (n*ratio)]
+X_train = X[:, :int(n*ratio)]
+Y_train = Y[:, :int(n*ratio)]
 
-X_test = X [:, int (n*ratio):]
-Y_test = Y [:, int (n*ratio):]
+X_test = X[:, int(n*ratio):]
+Y_test = Y[:, int(n*ratio):]
 
-plt.scatter(X_train[0,:], X_train[1,:], c=Y_train[0,:])
+plt.scatter(X_train[0, :], X_train[1, :], c=Y_train[0, :])
 plt.show()
 
 alpha = 0.001
